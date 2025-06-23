8d6eb1572188a8ecf2951bb7e87e73366e0814ee
Nils Ponsard <nils.ponsard@inria.fr>
2025-06-23 12:09:41 +0200

    fix toolchain install ?

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rustup component add --toolchain nightly clippy
+rustup component add --toolchain nightly-x86_64-unknown-linux-gnu clippy
 rustup target add thumbv8m.main-none-eabi --toolchain nightly 
 git clone https://github.com/ariel-os/ariel-os build/imports/ariel-os || true
